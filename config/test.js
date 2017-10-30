cd710087faf21e6020a359bfa9477f248a856949
BogdanNic <bogdan_nic100@yahoo.com>
2017-10-30 17:14:20 +0200

    Update test.js
    different trys to get variable input

--- config/test.js
+++ config/test.js
@@ -23,6 +23,47 @@ const puppeteer = require('puppeteer');
        })
     });
   });
+
+
+ ///almost working but no context to use the version
+                                // page.evaluateHandle(() => Promise.resolve(window)).then(aWindowHandle=>{
+                                //     page.evaluate(window => window.setVersion(version),aWindowHandle);
+                                // });
+
+
+                                ///almost working
+                                // page.$('#version').then( bodyHandle =>{
+                                //     console.log("in page",version);
+                                //     bodyHandle['version'] = version;
+								//
+                                //     function test(body) {
+                                //         //console.log(body);
+                                //         body.innerHTML = "123";
+                                //         //document.body.innerHTML="asd123";
+                                //     }
+								//
+                                //     page.on('console', msg => console.log('PAGE LOG:', ...msg.args));
+                                //     page.evaluate(test, bodyHandle).then(html=>{
+                                //       // console.log(html);
+								//
+                                //         page.screenshot({path: 'resources/example.png'}).then(()=>{
+                                //            browser.close();
+                                //         });
+                                //     });
+								//
+                                // });
+
+
+
+                                // page.evaluateHandle(() => ({window, document})).then(handle=>{
+                                //     handle.getProperties().then(properties=>{
+                                //         const windowHandle = properties.get('window');
+                                //         const documentHandle = properties.get('document');
+                                //        documentHandle.querySelector("#version").innertHTML ="ASDas";
+								//
+                                //     });
+                                //     console.log(document.querySelector("#version"));
+                                // });
 // puppeteer.launch().then(async browser => {
 //   const page = await browser.newPage();
 //   await page.goto('https://example.com');
